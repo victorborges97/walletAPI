ed34df895dd27502203021a38954376bf18fb84e
Jv Borges <borges.jvdo@gmail.com>
2022-03-20 14:24:04 -0300

    Configuração inicial para os teste

---
+++ src/test/java/com/borges/wallet/wallet/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.borges.wallet.wallet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorld {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals(1, 1);
+    }
+}
